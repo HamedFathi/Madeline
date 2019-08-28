fc720d539d001c178d161541898ae9036cf1dacb
hamedfathi <noeffect@outlook.com>
2019-08-28 14:04:05 +0430

    add: VariableExtractor

--- extractors/variable/VariableExtractor.ts
+++ extractors/variable/VariableExtractor.ts
@@ -1,12 +1,15 @@
 import { VariableStatement } from 'ts-morph';
 import { TypeExtractor } from '../common/TypeExtractor';
 import { VariableInfo } from './VariableInfo';
+import { TypescriptCommentExtractor } from '../comment/TypescriptCommentExtractor';
 
 export class VariableExtractor {
     public extract(node : VariableStatement): VariableInfo[] {
         let modifiers = node.getModifiers().map(x => x.getText());
         let kind = node.getDeclarationKind();
         let kindName = node.getDeclarationKind().toString();
+        let trailingComments = new TypescriptCommentExtractor().extract(node.getTrailingCommentRanges());
+        let leadingComments = new TypescriptCommentExtractor().extract(node.getLeadingCommentRanges());
         let variables = node.getDeclarationList().getDeclarations().map(x => {
             return {
                 name: x.getName(),
@@ -18,7 +21,9 @@ export class VariableExtractor {
                     ? undefined
                     : x.getInitializerOrThrow().getText(),
                 kind: kind,
-                kindName: kindName
+                kindName: kindName,
+                trailingComments: trailingComments.length === 0 ? undefined : trailingComments,
+                leadingComments: leadingComments.length === 0 ? undefined : leadingComments
             };
         });
         return variables;
--- extractors/variable/VariableInfo.ts
+++ extractors/variable/VariableInfo.ts
@@ -1,9 +1,12 @@
 import { VariableDeclarationKind } from 'ts-morph';
 import { TypeInfo } from '../common/TypeInfo';
+import { CommentInfo } from '../comment/CommentInfo';
 export interface VariableInfo {
     name: string;
     type: TypeInfo;
     modifiers: string[] | undefined;
     kind: VariableDeclarationKind;
     kindName: string;
+    trailingComments: CommentInfo[] | undefined;
+    leadingComments: CommentInfo[] | undefined;
 }

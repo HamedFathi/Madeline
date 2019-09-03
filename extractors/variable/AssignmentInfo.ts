36b1431c23fd09f59a5dfbf221a275cbd07b0a67
hamedfathi <noeffect@outlook.com>
2019-09-04 00:39:00 +0430

    update: AssignmentInfo

--- extractors/variable/AssignmentInfo.ts
+++ extractors/variable/AssignmentInfo.ts
@@ -1,9 +1,13 @@
 import { TypeInfo } from '../common/TypeInfo';
+import { VariableObjectLiteralInfo } from './VariableObjectLiteralInfo';
+import { VariableArrayLiteralInfo } from './VariableInitializerInfo';
+import { FunctionInfo } from '../function/FunctionInfo';
+import { CallSignatureInfo } from './CallSignatureInfo';
 
 export interface AssignmentInfo {
     name: string;
     type: TypeInfo;
-    value: any;
+    value: VariableObjectLiteralInfo | VariableArrayLiteralInfo | FunctionInfo | CallSignatureInfo | string | null | undefined;
     isShorthand: boolean;
     isSpread: boolean;
 }

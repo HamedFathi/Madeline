c19440f4e59d93c90aaf8c9eea7137b44524e799
hamedfathi <noeffect@outlook.com>
2019-09-12 11:05:31 +0430

    remove: *.js and *.map files from markdown folder

--- markdown/Const.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var Const = (function () {
-    function Const() {
-    }
-    return Const;
-}());
-exports.Const = Const;
-//# sourceMappingURL=Const.js.map

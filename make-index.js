05bbd982edcf1b366221f28ef8d18b7069db11b9
hamedfathi <noeffect@outlook.com>
2019-09-16 23:02:14 +0430

    add: make-index.js

---
+++ make-index.js
@@ -0,0 +1,18 @@
+const glob = require('glob');
+const fs = require('fs');
+
+glob("./*(extractors|templates|utilities)/**/*.ts", function (er, files) {
+    if (er) {
+        return console.log(err);
+    }
+    let path = [];
+    files.forEach(file => {
+        let newFile = file.replace('.ts', '');
+        path.push("export * from '" + newFile + "';");
+    });
+    fs.writeFile("./index.ts", path.join('\n'), function (err) {
+        if (err) {
+            return console.log(err);
+        }
+    });
+});

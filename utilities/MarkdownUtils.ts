cef443c1b61496fae38b0469fc2259a128758c0d
hamedfathi <noeffect@outlook.com>
2019-09-17 09:37:56 +0430

    chore(mocha): decrease timeout to 10s

--- utilities/MarkdownUtils.ts
+++ utilities/MarkdownUtils.ts
@@ -5,12 +5,13 @@ export class MarkdownUtils {
     private readonly BREAK_LINE = '\n';
     private readonly BREAK_LINES = '\n\n';
     public purify(text: string, trim = true): string {
-        const result = text
+        const content = text
             .split(this.BREAK_LINE)
             .map(x => (new StringUtils().isEmptyOrWhitespace(x) ? this.EMPTY : trim ? x.trim() : x))
             .join(this.BREAK_LINE)
             .replace(/\n{2,}/g, this.BREAK_LINES)
             .trim();
-        return result + this.BREAK_LINES;
+        const result = content + this.BREAK_LINES;
+        return result;
     }
 }

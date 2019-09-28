88ab13c7b64658da93885cce0d3fdc3231f67417
Saeed Ganji <shahabganji@gmail.com>
2019-09-28 23:38:50 +0330

    test(HtmlCommentExtractor): add another basic test :wink:

--- test/extractors/comment/HtmlCommentExtractor.spec.ts
+++ test/extractors/comment/HtmlCommentExtractor.spec.ts
@@ -2,13 +2,14 @@ import { assert } from 'chai';
 import { CommentInfo } from '../../../extractors/comment/CommentInfo';
 import { CommentKind } from '../../../extractors/comment/CommentKind';
 import { HtmlCommentExtractor } from '../../../extractors/comment/HtmlCommentExtractor';
+import { JsDocExtractor } from '../../../extractors/comment/JsDocExtractor';
 
 
 describe('HTML Extractor', () => {
-
-    it('should return the exact comment info returned by JsDocExtractor', () => {
-
-        const mockCommentInfo: CommentInfo = {
+    let mockCommentInfo: CommentInfo;
+    let mockJsDocExtractor: JsDocExtractor;
+    beforeEach(() => {
+        mockCommentInfo = {
             text: "Some Comment",
             description: ["This is a sample comment"],
             kind: CommentKind.Html,
@@ -22,11 +23,14 @@ describe('HTML Extractor', () => {
             }]
         };
 
-        const mockJsDocExtractor = {
+        mockJsDocExtractor = {
             extract: function (data: string, kind: CommentKind): CommentInfo {
                 return mockCommentInfo;
             }
         };
+    });
+
+    it('should return the exact comment info returned by JsDocExtractor', () => {
 
         const sut = `<template>
             <!-- Some Comment -->
@@ -41,4 +45,17 @@ describe('HTML Extractor', () => {
         assert.deepEqual(result, [mockCommentInfo]);
 
     });
+
+    it('should not call the extract method of jsdocExtractor when no comment is available', () => {
+
+        const sut = `<template>
+        <button>Click Me!</button>
+        </template>`;
+
+        const htmlCommentExtractor = new HtmlCommentExtractor(mockJsDocExtractor);
+
+        const result = htmlCommentExtractor.extract(sut);
+
+        assert.equal(result.length, 0);
+    })
 })

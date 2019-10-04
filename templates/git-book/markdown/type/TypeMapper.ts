a2845eade19ae44f760401ba61b3a87fc083fd18
hamedfathi <noeffect@outlook.com>
2019-10-05 01:50:21 +0330

    feat(TypeMapInfo) supports new packages

--- templates/git-book/markdown/type/TypeMapper.ts
+++ templates/git-book/markdown/type/TypeMapper.ts
@@ -1,9 +1,18 @@
 import { FromTypeInfo } from '../../../../extractors/common/FromTypeInfo';
 import { TypeMapInfo } from './TypeMapInfo';
 import { ExportedSourceFileInfo } from '../../../../extractors/source-file/ExportedSourceFileInfo';
-import { typeCategoryFinder } from './typeCategoryFinder';
+import { typeCategoryFinder } from './TypeCategoryFinder';
+/*
+"text": "import(\"D:/@Git/aurelia/packages/__e2e__/node_modules/@aurelia/runtime/node_modules/@aurelia/kernel/dist/di\").IRegistry",
+"text": "import(\"D:/@Git/aurelia/packages/__e2e__/node_modules/@aurelia/runtime/dist/binding/expression-parser\").BindingType",
 
-export const typeMapper = function(
+"text": "import(\"D:/@Git/aurelia/packages/i18n/src/utils\").BindingWithBehavior",
+"text": "import(\"D:/@Git/aurelia/packages/jit/src/attribute-pattern\").AttributePatternDefinition[]",
+
+"text": "import(\"D:/@Git/aurelia/node_modules/modify-code/index\").ModifyCodeResult",
+"text": "import(\"D:/@Git/aurelia/node_modules/@types/webpack/index\").loader.LoaderContext",
+*/
+export const typeMapper = function (
     id: string,
     from: FromTypeInfo[],
     source: ExportedSourceFileInfo,
@@ -17,8 +26,26 @@ export const typeMapper = function(
         if (baseUrl) {
             pathParts.push(baseUrl);
         }
-        if (f.path.includes('packages')) {
-            const parts = f.directory
+
+        if (f.directory.includes('@aurelia') && f.directory.includes('dist')) {
+            let dir = f.directory.substr(f.directory.lastIndexOf('@aurelia'));
+            const parts = dir
+                .split('@aurelia')[1]
+                .split('/')
+                .filter(x => x !== '' && x !== 'dist');
+            parts.forEach(p => {
+                pathParts.push(p.toLowerCase());
+            });
+            pathParts.push(f.file.toLowerCase());
+            if (category) {
+                pathParts.push(category.toLowerCase());
+            }
+            pathParts.push(f.type.toLowerCase());
+
+        }
+        if (f.directory.includes('packages') && f.directory.includes('src')) {
+            let dir = f.directory.substr(f.directory.lastIndexOf('packages'));
+            const parts = dir
                 .split('packages')[1]
                 .split('/')
                 .filter(x => x !== '' && x !== 'src');
@@ -31,8 +58,21 @@ export const typeMapper = function(
             }
             pathParts.push(f.type.toLowerCase());
         }
-        if (f.directory.includes('node_modules')) {
-            const parts = f.directory
+        if (f.directory.includes('node_modules') && !f.directory.includes('dist') && !f.directory.includes('src') && !f.directory.includes('@types')) {
+            let dir = f.directory.substr(f.directory.lastIndexOf('packages'));
+            const parts = dir
+                .split('node_modules')[1]
+                .split('/')
+                .filter(x => x !== '');
+            parts.forEach(p => {
+                pathParts.push(p.toLowerCase());
+            });
+            pathParts.push(f.type);
+            fromNodeModules = true;
+        }
+        if (f.directory.includes('node_modules') && !f.directory.includes('dist') && !f.directory.includes('src') && f.directory.includes('@types')) {
+            let dir = f.directory.substr(f.directory.lastIndexOf('packages'));
+            const parts = dir
                 .split('node_modules')[1]
                 .split('/')
                 .filter(x => x !== '');

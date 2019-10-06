dbdca8ff9f4d9c044b7bba648e6aac6fb0009aa0
hamedfathi <noeffect@outlook.com>
2019-10-06 14:36:18 +0330

    feat(SetAccessorsToMdConverter) return md text.

--- templates/git-book/markdown/class/SetAccessorTemplate.ts
+++ templates/git-book/markdown/class/SetAccessorTemplate.ts
@@ -1,2 +1,35 @@
 export const SET_ACCESSOR_TEMPLATE = `
+{% if description|is_available %}
+    **Summary:** 
+
+    {{description|print('','','','\n\n')}}
+
+{% endif %}
+
+**Name:**      
+
+{{name|print('-','','')}}
+
+**Type:**      
+
+{{type|print('-','','')}}
+
+{% if modifiers|is_available %}
+    **Modifier(s):** 
+
+    {{modifiers|print_modifiers}}
+
+{% endif %}
+
+{% if typeParameters|is_available %}
+    **Type Parameter(s):** 
+
+    {{typeParameters|print('','\`\`\`','\`\`\`','\n\n')}}
+
+{% endif %}
+
+**Parameter Name:** {{parameter.name|print('-','','')}}
+
+**Parameter Type:** {{parameter.type|print('-','','')}}
+
 `;
